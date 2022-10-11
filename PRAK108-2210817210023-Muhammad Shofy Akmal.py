77576d1ee23b99accf2676312dcea4843af4181e
AkmalShofy <114986742+AkmalShofy@users.noreply.github.com>
2022-10-11 17:29:43 +0800

    Add files via upload

--- PRAK108-2210817210023-Muhammad Shofy Akmal.py
+++ PRAK108-2210817210023-Muhammad Shofy Akmal.py
@@ -2,7 +2,7 @@ a,b,phi=5,14,3.14
 c=b/a
 d=c/(phi*2)
 print ("Diketahui :")
-print ("Pak Dengklek mengelilingi taman = ", a)
-print ("Jarak tempuh Pak Dengklek = ", b)
+print ("Pak Dengklek mengelilingi taman = ", a,"Putaran")
+print ("Jarak tempuh Pak Dengklek = ", b,"Kilometer")
 print ("Jawaban :")
 print ("Jari-jari taman yang dikelilingi Pak Dengklek adalah ", round(d,2),"Kilometer")
--- PRAK108-2210817210023-MuhammadShofyAkmal.c
+++ PRAK108-2210817210023-MuhammadShofyAkmal.c
@@ -4,8 +4,8 @@ float a=5, b=14, phi=3.14, c, d;
 c=b/a;
 d=c/(phi*2);
 printf ("Diketahui :\n");
-printf ("Pak Dengklek mengelilingi taman = %fPutaran\n", a);
-printf ("Jarak tempuh Pak Dengklek = %f Kilometer\n", b);
+printf ("Pak Dengklek mengelilingi taman = %.0f Putaran\n", a);
+printf ("Jarak tempuh Pak Dengklek = %.0f Kilometer\n", b);
 printf ("Jawaban :\n");
 printf ("Jari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer", d);  
 }
